301afa09ccb2d8a6fa384218c8d5ea1a1f2a47d8
Gustavo Lopez <gustavo@getkalto.com>
2023-02-03 18:40:47 -0300

    Revert "Several libraries were added related to MUI"
    This reverts commit 1a7b009c93c6bbb8e606fd133ed14dd130ec4480.

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Header from './header';
-import Menu from './menu';
-import Content from './content';
-import Footer from './footer';
-
-export { Header, Menu, Content, Footer };
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import About from './About';
-import Contact from './Contact';
-import Home from './Home';
-
-export { About, Contact, Home };
